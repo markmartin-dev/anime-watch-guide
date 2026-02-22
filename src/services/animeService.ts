6d3d67d0aea6d774b2df00c218de5e0f7d37f03c
Mark Martin <development@roentgenmartin.com>
2026-02-22 07:15:48 -0500

    added parameters for anime, removed nsfw

--- src/services/animeService.ts
+++ src/services/animeService.ts
@@ -1,8 +1,8 @@
 import { apiGet } from '../api/client'
 import type { AnimeListResponse, Anime } from '../types/anime'
 
-export const fetchAnimeList = async (page = 1): Promise<AnimeListResponse> => {
-  return apiGet('/anime', { page })
+export const fetchAnimeList = async (page = 1, limit = 20, orderBy = 'title', sfw = true, type = "tv"): Promise<AnimeListResponse> => {
+  return apiGet('/anime', { page, limit, orderBy, sfw, type })
 }
 
 export const fetchAnimeById = async (id: string | number): Promise<{ data: Anime }> => {
