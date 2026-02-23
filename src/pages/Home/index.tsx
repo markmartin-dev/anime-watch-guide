72af9ff0d6efcb498520c1e1457627f75a71b0c0
Mark Martin <development@roentgenmartin.com>
2026-02-22 22:03:39 -0500

    add more sections to Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from '../../components/layout/Header'
+import { AnimeCarousel } from '../../components/carousel/AnimeCarousel'
+import { useTopAnime } from '../../hooks/useAnime'
 
 const Home: React.FC = () => {
+    const { data } = useTopAnime({ page: 1, limit: 10, sfw: true, filter: 'airing', type: 'tv' })
+    const popularAnime = data?.data || []
   return (
     <div>
       <Header />
       <main>
-        <h1>Anime Watch Guide</h1>
-        <div>Carousel from popular anime</div>
+        <div>
+            <h1>Anime Watch Guide</h1>
+            <AnimeCarousel slides={popularAnime.map(a => ({id: a.mal_id.toString(), title: a.title, synopsis: a.synopsis, images: a.images}))}/>
+        </div>
         <div>
             <h2>News</h2>
             <span>anime news goes here</span>
